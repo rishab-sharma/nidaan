91d267889ddf59ff08aa22858d9408f26311dc0d
rishabsharma <rishabsharma@gofynd.com>
2019-04-18 10:17:05 +0530

    New Code

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+from inference import get_inference
+from flask import Flask , render_template,request
+from keras.applications.vgg19 import preprocess_input
+from keras.preprocessing import image
+import numpy as np
+
+app = Flask(__name__, template_folder = 'templates', static_url_path = "/static")
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/')  
+def index():
+    return render_template('upload.html') 
+
+@app.route('/upload',methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT,'images/')
+    print(target)
+
+    if not os.path.isdir(target):
+    	os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+
+        #filename = file.filename
+        destintion = "/".join([target,'test.jpeg'])
+        print(destintion)
+        file.save(destintion)
+        
+
+    return render_template('complete.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    img = image.load_img('images/test.jpeg',target_size=(512,512))
+    im = image.img_to_array(img)
+    im = preprocess_input(im)
+    im = np.expand_dims(im,axis=0)
+    prediction = get_inference(im)
+    print(prediction)
+    return (str(prediction))
+  
+if __name__=='__main__':
+   app.run(port = 4555,debug=True)         	
---
+++ inference.py
@@ -0,0 +1,36 @@
+from keras.layers import *
+from keras.models import *
+import numpy as np
+from PIL import Image
+import os
+
+def get_inference(img):
+    model = Sequential()
+    model.add(Conv2D(64, kernel_size = (3,3), strides = (1,1), activation = 'relu', padding='valid',input_shape = (512,512,3),      name='block1_conv1'))
+    model.add(Conv2D(64, kernel_size = (3,3), strides = (1,1), activation = 'relu',padding='valid', name='block1_conv2'))
+    model.add(MaxPooling2D(pool_size=(2,2),strides = (2,2),padding='valid',name='block1_pool'))
+
+    model.add(Conv2D(128, kernel_size = (3,3), strides = (1,1), activation = 'relu', padding='valid', name='block2_conv1'))
+    model.add(Conv2D(128, kernel_size = (3,3), strides = (1,1), activation = 'relu',padding='valid', name='block2_conv2'))
+    model.add(MaxPooling2D(pool_size=(2,2),strides = (2,2),padding='valid',name='block2_pool'))
+
+    model.add(Conv2D(256, kernel_size = (3,3), strides = (1,1), activation = 'relu', padding='valid', name='block3_conv1'))
+    model.add(Conv2D(256, kernel_size = (3,3), strides = (1,1), activation = 'relu',padding='valid', name='block3_conv2'))
+    model.add(MaxPooling2D(pool_size=(2,2),strides = (2,2),padding='valid',name='block3_pool'))
+
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(1028, activation = 'relu'))
+    model.add(Dense(512, activation = 'relu'))
+    model.add(Dense(1, activation = 'sigmoid'))
+    model.load_weights('Pneumonia_model.h5')
+    prediction = model.predict(img)
+    return prediction
+    
+if __name__=='__main__':
+   img = image.load_img('test.jpeg',target_size=(512,512))
+   im = image.img_to_array(img)
+   im = preprocess_input(im)
+   im = np.expand_dims(im,axis=0)
+   prediction = get_inference(im)
+   print(prediction)
+   
