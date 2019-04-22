acb15f0a33a3eb154b84cbef9b127a84e687088a
rishabsharma <rishabsharma@gofynd.com>
2019-04-22 09:29:42 +0530

    Tuberclosis Model

--- app.py
+++ app.py
@@ -24,18 +24,23 @@ def upload():
         destintion = "/".join([target,'test.jpeg'])
         print(destintion)
         file.save(destintion)
+    print(file.filename)
+    img = image.load_img('images/test.jpeg', target_size=(512, 512))
 
-    return make_response("uploaded")
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    img = image.load_img('images/test.jpeg',target_size=(512,512))
     im = image.img_to_array(img)
+
     im = preprocess_input(im)
-    im = np.expand_dims(im,axis=0)
-    prediction = get_inference(im)
-    print(prediction)
-    return (str(prediction))
+
+    im = np.expand_dims(im, axis=0)
+
+    prediction = get_inference(im)[0][0]
+
+    if prediction == 1.0:
+        result = "You Diagnosis show Postive Presence of the Chest Infection of Tuberclosis"
+    else:
+        result = "You Diagnosis show Negative Presence of the Chest Infection of Tuberclosis"
+    return render_template('result.html', result = result)
+
   
 if __name__=='__main__':
    app.run(host = '127.0.0.1', port = 5000,debug=True)         	
